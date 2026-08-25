270e1b4ca018b16d6da4a42f6bae27f9688f7f46
shpeckman <geelenrobin@proton.me>
2026-08-25 16:43:45 +0200

    refactor(pty): inline UTF8 filter into filter.cr

--- src/pty.cr
+++ src/pty.cr
@@ -1,7 +1,6 @@
 # src/pty.cr
 require "./pty/win_size"
 require "./pty/filter"
-require "./pty/utf8_filter"
 require "./pty/ansi"
 require "./pty/session"
 require "./pty/pool"
--- src/pty/filter.cr
+++ src/pty/filter.cr
@@ -65,4 +65,61 @@ module PTY
       on_line(rest)
     end
   end
+
+  module UTF8
+    class Filter < ::PTY::Filter
+      @incomplete : Bytes = Bytes.empty
+
+      def call(chunk : Bytes) : Bytes
+        return Bytes.empty if chunk.empty? && @incomplete.empty?
+
+        if @incomplete.empty?
+          data = chunk
+        else
+          data = Bytes.new(@incomplete.size + chunk.size)
+          @incomplete.copy_to(data[0, @incomplete.size])
+          chunk.copy_to(data[@incomplete.size, chunk.size])
+          @incomplete = Bytes.empty
+        end
+
+        split_idx = data.size
+        i         = 1
+
+        while i <= 4 && i <= data.size
+          byte = data[data.size - i]
+
+          if byte < 0x80_u8
+            break
+          elsif byte >= 0xC0_u8
+            expected = if byte >= 0xF0_u8
+                         4
+                       elsif byte >= 0xE0_u8
+                         3
+                       else
+                         2
+                       end
+
+            split_idx = data.size - i if expected > i
+            break
+          end
+
+          i += 1
+        end
+
+        if split_idx < data.size
+          tail_size   = data.size - split_idx
+          @incomplete = Bytes.new(tail_size)
+          data[split_idx, tail_size].copy_to(@incomplete)
+        end
+
+        data[0, split_idx]
+      end
+
+      def finish : Bytes
+        rest        = @incomplete
+        @incomplete = Bytes.empty
+        rest
+      end
+    end
+  end
 end
